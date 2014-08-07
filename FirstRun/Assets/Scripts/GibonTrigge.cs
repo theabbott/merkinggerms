f15e8aabe363d7ea702fb302c48f4022317bfaa6
J A R GILBERTSON <joe.gilbertson@gmail.com>
2014-08-08 01:29:26 +0800

    Splershins!

---
+++ FirstRun/Assets/Scripts/GibonTrigge.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibonTrigge : MonoBehaviour {
+
+	public GameObject gib;
+
+
+	void OnTriggerEnter()
+	{
+
+		//Debug.Log ("We've got  this shit");
+		Instantiate (gib, transform.position, Quaternion.identity);
+		Destroy(gameObject);
+
+	}
+
+
+
+	
+
+}
