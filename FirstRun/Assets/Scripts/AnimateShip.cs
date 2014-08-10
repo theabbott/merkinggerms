0ee473442e4bb9a87251ef226797d147eb50afe8
J A R GILBERTSON <joe.gilbertson@gmail.com>
2014-08-10 23:16:53 +0800

    Sherps!

---
+++ FirstRun/Assets/Scripts/AnimateShip.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateShip : MonoBehaviour {
+
+
+		
+	public float turnrate = 1.0f;
+	public float og1 = 1.0f; 
+	public float og2 = 1.0f;
+	public float og3 = 1.0f;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	//rolls the ship left n right on player input.
+
+		if(Input.GetButton ("Horizontal"))
+					
+			transform.Rotate(-Input.GetAxis("Horizontal"),0,turnrate*-Input.GetAxis("Horizontal")); 
+
+
+		else
+
+		    //transform.position= (Vector3(1.2,1.1,-47.2));
+			transform.position=new Vector3 (1,1, -47);
+			
+
+		
+				 
+				 
+
+	
+	}
+}
---
+++ FirstRun/Assets/Scripts/PseudoLeftRight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PseudoLeftRight : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () 
+	
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	
+	{
+		renderer.material.mainTextureOffset += Vector2.right*Input.GetAxis("Horizontal")* speed * Time.deltaTime ;
+
+	}
+}
