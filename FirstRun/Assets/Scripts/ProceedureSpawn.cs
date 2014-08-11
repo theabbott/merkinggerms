73ba78a7bb55bf2a4016cfc649158e173238801f
J A R GILBERTSON <joe.gilbertson@gmail.com>
2014-08-12 02:50:39 +0800

    Grewing Pwerlers!

---
+++ FirstRun/Assets/Scripts/ProceedureSpawn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProceedureSpawn : MonoBehaviour
+{
+	public GameObject thingy;
+	public float Minhorizontalpos = 10.0f;
+	public float Maxhorizontalpos = 10.0f;
+	public float Maxverticalpos = 10.0f;
+	public float Minverticalpos = 10.0f;
+
+	public float minspawnrate = 10.0f; 
+	public float Maxspawnrate = 1.0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		//Invoke("SpawnNow", Random.Range(minspawnrate, Maxspawnrate)); 
+	
+	}
+	
+
+
+	void Update() 
+	{
+		Instantiate(thingy,transform.position + new Vector3(Random.Range(Minhorizontalpos,Maxhorizontalpos),Random.Range(Minverticalpos,Maxverticalpos)), Quaternion.identity);
+	
+		//Invoke("SpawnNow", Random.Range(minspawnrate, Maxspawnrate)); 
+	
+	}
+
+}
---
+++ FirstRun/Assets/Scripts/ScaleOverTime.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleOverTime : MonoBehaviour 
+{
+
+	public Vector3 finalscale = Vector3.zero; 
+	private Vector3 initialscale;
+	public float time = 1.0f;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+		initialscale = transform.localScale;
+		StartCoroutine ("Scale");
+
+
+	}
+	
+	// Function coroutine to scale obstacles.
+	IEnumerator Scale () 
+	{
+	
+		float t = 0.0f;
+		while (t<= time) 
+		{	
+			transform.localScale=Vector3.Lerp(initialscale,finalscale,t/time);
+			t += Time.deltaTime;
+			yield return null;
+		}
+
+		transform.localScale = finalscale;
+
+	
+	}
+}
