183094ce8377fbb4a6020d6656f11b78af1258dc
J A R GILBERTSON <joe.gilbertson@gmail.com>
2014-08-13 02:21:52 +0800

    Highsceres!

---
+++ FirstRun/Assets/Scripts/MoveGamespeed.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveGamespeed : MonoBehaviour {
+
+	public Vector3 direction = Vector3.forward;
+	public float OGSpeed; 
+	// Use this for initialization
+	void Start () 
+	{
+	
+		OGSpeed = GameManager.instance.gamespeed;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//transform.position += Vector3.right;
+		transform.position += transform.rotation*(direction.normalized*GameManager.instance.gamespeed*Time.deltaTime);
+		if (GameManager.instance.gamespeed>=0)
+			GameManager.instance.gamespeed += 0.1f*Input.GetAxis("Vertical"); 
+		
+		if (GameManager.instance.gamespeed < 0)
+			GameManager.instance.gamespeed = OGSpeed;
+		
+		if(Input.GetButtonUp("Vertical"))
+			
+			if (GameManager.instance.gamespeed>OGSpeed)
+				GameManager.instance.gamespeed -= 1.0f;
+
+
+
+		//print (transform.position);
+
+
+	}
+
+
+}
