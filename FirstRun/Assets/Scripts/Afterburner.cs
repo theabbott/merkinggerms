83fa8a4ebfaf4f3f68b156d07996ca4410f691e6
J A R GILBERTSON <joe.gilbertson@gmail.com>
2014-08-13 00:55:29 +0800

    ITs a Germ!

---
+++ FirstRun/Assets/Scripts/Afterburner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Afterburner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	
+	{
+	
+		particleSystem.enableEmission = false;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	
+	{
+	
+		if(Input.GetButtonDown("Vertical"))
+
+			particleSystem.enableEmission = true;
+		if(Input.GetButtonUp("Vertical"))
+
+			particleSystem.enableEmission = false;
+	}
+}
---
+++ FirstRun/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	
+	private bool gameover = false;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+		
+		
+	{
+		if(GameObject.FindGameObjectWithTag ("Player") == null)
+		   {
+			
+			
+			gameover = true; 
+			
+		}
+		
+		if (gameover)
+		{
+			
+			if(Input.anyKeyDown)
+			{
+				Application.LoadLevel(Application.loadedLevel);
+			}
+			
+			
+		}
+		
+		
+		
+		}
+		
+		void OnGUI () 
+		
+		
+		{
+			if (gameover == true)
+				GUILayout.Label ("ITS OVER! PRESS ENTER TO TRY AGAIN");
+		}
+		
+		
+		}
+
