e0847b61ba63c51ab771f2f5b9dee0b5798114be
J A R GILBERTSON <joe.gilbertson@gmail.com>
2014-08-15 03:42:54 +0800

    terch!

---
+++ FirstRun/Assets/Scripts/LoadLevelOnTouched.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelOnTouched : MonoBehaviour 
+{
+
+	public string levelname = "Level1";
+
+
+
+
+	// Use this for initialization
+	void Touched () 
+	
+	
+	{
+	
+		Application.LoadLevel (levelname); 
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	
+	{
+
+
+		if(Input.touchCount>=2)
+			Application.LoadLevel (levelname); 
+			//Application.LoadLevel (levelname);
+	
+	}
+}
